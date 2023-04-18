4300717d6034e8211f66ba68faa693f088bf1a67
Jim <40590729+Whalesea-Gustav@users.noreply.github.com>
2023-04-18 16:11:38 +0800

    finish complex maps

--- Assets/CustomRP/Shaders/LitInput.hlsl
+++ Assets/CustomRP/Shaders/LitInput.hlsl
@@ -10,6 +10,7 @@ TEXTURE2D(_EmissionMap);
 SAMPLER(sampler_BaseMap);
 
 TEXTURE2D(_DetailMap);
+TEXTURE2D(_DetailNormalMap);
 SAMPLER(sampler_DetailMap);
 
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
@@ -25,9 +26,10 @@ UNITY_DEFINE_INSTANCED_PROP(float, _Smoothness)
 UNITY_DEFINE_INSTANCED_PROP(float, _Fresnel)
 UNITY_DEFINE_INSTANCED_PROP(float, _DetailAlbedo)
 UNITY_DEFINE_INSTANCED_PROP(float, _DetailSmoothness)
+UNITY_DEFINE_INSTANCED_PROP(float, _DetailNormalScale)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
-float2 TransformBaseUV(float2 baseUV)
+float2 TransformBaseUV (float2 baseUV)
 {
     float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
     return baseUV * baseST.xy + baseST.zw;
@@ -45,21 +47,10 @@ float4 GetBase(float2 baseUV)
     return map * color;
 }
 
-float4 GetDetail (float2 detailUV) {
+float4 GetDetail(float2 detailUV) {
     float4 map = SAMPLE_TEXTURE2D(_DetailMap, sampler_DetailMap, detailUV);
     return map * 2.0 - 1.0;
     return map;
-}
-
-float4 GetBase (float2 baseUV, float2 detailUV) {
-    float4 map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, baseUV);
-    float4 color = INPUT_PROP(_BaseColor);
-	
-    float detail = GetDetail(detailUV).r * INPUT_PROP(_DetailAlbedo);
-    map.rgb = lerp(sqrt(map.rgb), detail < 0.0 ? 0.0 : 1.0, abs(detail));
-    map.rgb *= map.rgb;
-	
-    return map * color;
 }
 
 float4 GetMask(float2 baseUV)
@@ -67,10 +58,35 @@ float4 GetMask(float2 baseUV)
     return SAMPLE_TEXTURE2D(_MaskMap, sampler_BaseMap, baseUV);
 }
 
+float4 GetBase (float2 baseUV, float2 detailUV) {
+    float4 map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, baseUV);
+    float4 color = INPUT_PROP(_BaseColor);
+	
+    float detail = GetDetail(detailUV).r * INPUT_PROP(_DetailAlbedo);
+    float mask = GetMask(baseUV).b;
+    map.rgb = lerp(sqrt(map.rgb), detail < 0.0 ? 0.0 : 1.0, abs(detail) * mask);
+    map.rgb *= map.rgb;
+	
+    return map * color;
+}
+
 float3 GetNormalTS (float2 baseUV) {
     float4 map = SAMPLE_TEXTURE2D(_NormalMap, sampler_BaseMap, baseUV);
     float scale = INPUT_PROP(_NormalScale);
     float3 normal = DecodeNormal(map, scale);
+    return normal;
+}
+
+float3 GetNormalTS (float2 baseUV, float2 detailUV) {
+    float4 map = SAMPLE_TEXTURE2D(_NormalMap, sampler_BaseMap, baseUV);
+    float scale = INPUT_PROP(_NormalScale);
+    float3 normal = DecodeNormal(map, scale);
+
+    map = SAMPLE_TEXTURE2D(_DetailNormalMap, sampler_DetailMap, detailUV);
+    scale = INPUT_PROP(_DetailNormalScale) * GetMask(baseUV).b;
+    float3 detail = DecodeNormal(map, scale);
+    normal = BlendNormalRNM(normal, detail);
+
     return normal;
 }
 
@@ -121,6 +137,127 @@ float GetOcclusion (float2 baseUV) {
     float occlusion = GetMask(baseUV).g;
     occlusion = lerp(occlusion, 1.0, strength);
     return occlusion;
+}
+
+//InputConfig Function
+
+struct InputConfig {
+    float2 baseUV;
+    float2 detailUV;
+    bool useMask;
+    bool useDetail;
+};
+
+InputConfig GetInputConfig (float2 baseUV, float2 detailUV = 0.0) {
+    InputConfig c;
+    c.baseUV = baseUV;
+    c.detailUV = detailUV;
+    c.useMask = false;
+    c.useDetail = false;
+    return c;
+}
+
+float4 GetMask(InputConfig c)
+{
+    if (c.useMask)
+    {
+        return SAMPLE_TEXTURE2D(_MaskMap, sampler_BaseMap, c.baseUV);
+    }
+    else
+    {
+        return 1.0;
+    }
+}
+
+float4 GetDetail(InputConfig c)
+{
+    if (c.useDetail)
+    {
+        float4 map = SAMPLE_TEXTURE2D(_DetailMap, sampler_DetailMap, c.detailUV);
+        return map * 2.0 - 1.0;
+    }
+    else
+    {
+        return 0.0;
+    }
+}
+
+
+float4 GetBase (InputConfig c)
+{
+    float4 map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, c.baseUV);
+    float4 color = INPUT_PROP(_BaseColor);
+
+    if (c.useDetail) {
+        float detail = GetDetail(c).r * INPUT_PROP(_DetailAlbedo);
+        float mask = GetMask(c).b;
+        map.rgb =
+            lerp(sqrt(map.rgb), detail < 0.0 ? 0.0 : 1.0, abs(detail) * mask);
+        map.rgb *= map.rgb;
+    }
+	
+    return map * color;
+}
+
+float3 GetNormalTS (InputConfig c) {
+    float4 map = SAMPLE_TEXTURE2D(_NormalMap, sampler_BaseMap, c.baseUV);
+    float scale = INPUT_PROP(_NormalScale);
+    float3 normal = DecodeNormal(map, scale);
+
+    if(c.useDetail)
+    {
+        map = SAMPLE_TEXTURE2D(_DetailNormalMap, sampler_DetailMap, c.detailUV);
+        scale = INPUT_PROP(_DetailNormalScale) * GetMask(c.baseUV).b;
+        float3 detail = DecodeNormal(map, scale);
+        normal = BlendNormalRNM(normal, detail);
+    }
+    
+    return normal;
+}
+
+float GetSmoothness (InputConfig c) {
+    float smoothness = INPUT_PROP(_Smoothness);
+    smoothness *= GetMask(c.baseUV).a;
+
+    if(c.useDetail)
+    {
+        float detail = GetDetail(c.detailUV).b * INPUT_PROP(_DetailSmoothness);
+        float mask = GetMask(c.baseUV).b;
+        smoothness = lerp(smoothness, detail < 0.0 ? 0.0 : 1.0, abs(detail) * mask);
+    }
+    
+    return smoothness;
+}
+
+float GetMetallic(InputConfig c)
+{
+    float metallic = INPUT_PROP(_Metallic);
+    metallic *= GetMask(c.baseUV).r;
+    return metallic;
+}
+
+float3 GetEmission(InputConfig c)
+{
+    float4 map = SAMPLE_TEXTURE2D(_EmissionMap, sampler_BaseMap, c.baseUV);
+    float4 color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _EmissionColor);
+    return map.rgb * color.rgb;
+}
+
+float GetOcclusion (InputConfig c) {
+    float strength = INPUT_PROP(_Occlusion);
+    float occlusion = GetMask(c.baseUV).g;
+    occlusion = lerp(occlusion, 1.0, strength);
+    return occlusion;
+}
+
+float GetFresnel(InputConfig c)
+{
+    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Fresnel);
+}
+
+float GetCutoff(InputConfig c)
+{
+    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff);
 }
 
 #endif
